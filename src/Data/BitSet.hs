e09ab63f4ea08310811eee86a8531fa29ecc3cba
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-03-08 15:29:54 +0000

    Use IntSet in BitSet

--- src/Data/BitSet.hs
+++ src/Data/BitSet.hs
@@ -6,20 +6,19 @@ module Data.BitSet
     , member
     ) where
 
-import Data.Bits
-import Data.Word
+import qualified Data.IntSet as IntSet
 
-data BitSet a = BitSet (a -> Int) Integer
+data BitSet a = BitSet (a -> Int) IntSet.IntSet
 
 empty :: (a -> Int) -> BitSet a
-empty f = BitSet f zeroBits
+empty f = BitSet f IntSet.empty
 
 insert :: a -> BitSet a -> BitSet a
-insert v (BitSet f b) = BitSet f $ setBit b $ f v
+insert v (BitSet f b) = BitSet f $ IntSet.insert (f v) b
 
 remove :: a -> BitSet a -> BitSet a
-remove v (BitSet f b) = BitSet f $ clearBit b $ f v
+remove v (BitSet f b) = BitSet f $ IntSet.delete (f v) b
 
 member :: a -> BitSet a -> Bool
-member v (BitSet f b) = testBit b $ f v
+member v (BitSet f b) = IntSet.member (f v) b
 
