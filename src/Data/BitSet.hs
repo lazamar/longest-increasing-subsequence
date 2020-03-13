8eee53c15ac785fab3239a8eec1e2de76f9ec645
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-03-13 01:13:58 +0000

    Use Vector (2x speedup)

--- src/Data/BitSet.hs
+++ src/Data/BitSet.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+
 module Data.BitSet
     ( BitSet
     , empty
@@ -6,20 +8,25 @@ module Data.BitSet
     , member
     ) where
 
-import Data.Bits
-import Basement.Types.Word256 (Word256)
+--import Data.Bits
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Generic.Mutable as V (write)
 
-newtype BitSet = BitSet Word256
+--newtype BitSet = BitSet (MuArray (Offset Bool) (CountOf Bool))
+newtype BitSet = BitSet (V.Vector Bool)
 
 empty :: BitSet
-empty = BitSet zeroBits
+empty = BitSet $ V.fromList $ fmap (const False) [1..255]
 
 insert :: Int -> BitSet -> BitSet
-insert v (BitSet b) = BitSet $ setBit b v
+insert v (BitSet b) = BitSet $ setBit True b v
 
 remove :: Int -> BitSet -> BitSet
-remove v (BitSet b) = BitSet $ clearBit b v
+remove v (BitSet b) = BitSet $ setBit False b v
 
 member :: Int -> BitSet -> Bool
 member v (BitSet b) = testBit b v
 
+
+setBit value vector n = V.modify (\v' -> V.write v' n value) vector
+testBit  vector n = vector V.! n
